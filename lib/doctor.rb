1bd50caf2a1249fce7e6bcdc853f14567dffc8aa
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 18:54:35 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,8 @@ class Doctor
     self.appointments = []
   end 
   
-  def new_appointment(date, patient) 
-    Appointment.new(patient, date, self).tap {|apt| self.appointments << apt}
+  def new_appointment(patient, date ) 
+    Appointment.new(date, patient, self).tap {|apt| self.appointments << apt}
   end 
   
   def patients 
