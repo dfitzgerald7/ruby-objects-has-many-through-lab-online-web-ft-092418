98ed2096c9011055a18a6b81355a99a9f176d1c8
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 18:31:40 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,6 +13,10 @@ class Genre
     Song.all.select {|song| song.genre == self}
   end 
   
+  def artists 
+    self.songs.map {|song| song.artists}.uniq
+  end 
+  
   def self.all 
     @@all 
   end 
