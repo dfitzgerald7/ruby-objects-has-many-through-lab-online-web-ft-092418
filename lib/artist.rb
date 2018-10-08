86be266e483ac2928187e5bef3591e1b7cb79414
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 17:52:35 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require "pry"
+class Artist 
+  attr_accessor :name
+  attr_writer :songs 
+  @@all = []
+  
+  def initialize(name)
+    self.name = name 
+    self.songs = []
+    @@all << self
+  end 
+  
+  def new_song(name, genre)
+   # binding.pry
+    song = Song.new(name, self, genre)
+    self.songs << song
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+  def genres 
+    genre_arr = []
+    self.songs.each {|song| genre_arr << song.genre unless genre_arr.include?(song.genre)}
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    @all << self 
+  end 
+  
+end 
