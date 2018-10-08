d382356d168e832b856952c459d87142717e653a
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 19:00:44 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,10 +3,11 @@ require "pry"
 class Patient
   
   attr_accessor :name, :appointments
-  
+  @@all = []
   def initialize(name)
     self.name = name
     self.appointments = [] 
+    @@all << self 
   end 
   
   def new_appointment(doctor, date)
