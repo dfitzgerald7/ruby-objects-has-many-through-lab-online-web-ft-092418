f831b98ecb4921651201529c9978b1b3adc027c6
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 18:53:20 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -6,7 +6,11 @@ class Appointment
     self.date = date 
     self.patient = patient 
     self.doctor = doctor
-    @@all << self
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
   end 
   
 end 
