b828997c55f79f452c6bdbc74a0568f7a73470d2
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 19:00:17 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,15 +2,16 @@ require "pry"
 
 class Patient
   
-  attr_accessor :name 
+  attr_accessor :name, :appointments
   
   def initialize(name)
     self.name = name
+    self.appointments = [] 
   end 
   
   def new_appointment(doctor, date)
     #binding.pry
-    Appointment.new(date, self, doctor) #.tap
+    Appointment.new(date, self, doctor).tap {|apt| self.appointments << apt}
   end 
   
 end 
