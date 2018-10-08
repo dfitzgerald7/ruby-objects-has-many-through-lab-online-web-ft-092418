916fffbbb2ea0fba40249b438a599266d085d0fa
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-10-08 18:39:50 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,7 +8,7 @@ class Doctor
   end 
   
   def new_appointment(date, patient) 
-    Appointment.new(date, patient).tap {|apt| self.appointments << apt}
+    Appointment.new(date, patient, self).tap {|apt| self.appointments << apt}
   end 
   
   def self.all 
